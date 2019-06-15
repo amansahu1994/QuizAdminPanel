7d35aaf881d68cd3ce22a1df65827bd59830cb16
Aman Sahu <asahu@knowledgeops.co.in>
2019-06-15 12:12:24 +0530

    aman

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -92,8 +92,7 @@
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Manage Test Questions</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa fa-fort-awesome"></i><a href="/" target="_self">Font Awesome</a></li>
-                            <li><i class="menu-icon ti-themify-logo"></i><a href="/" target="_self">Themefy Icons</a></li>
+                            <li><i class="menu-icon fa fa-th"></i><a href="/viewQuestions" target="_self">View All Test Questions</a></li>
                         </ul>
                     </li>
                 </ul>
